55ffbe2af778efeb4446ab2910ffd17c22fe2e33
Gordon Krefting <gordon@krefting.org>
2018-01-18 16:58:23 -0500

    indentation cleanup

--- src/Grid.js
+++ src/Grid.js
@@ -17,34 +17,34 @@ export default class Grid extends React.Component {
     }
   }
 
-	render() {
-		var o = [];
-		let cells = this.props.gridModel.cells;
+  render() {
+    var o = [];
+    let cells = this.props.gridModel.cells;
 
-		for (let i=0; i<cells.length;i++) {
+    for (let i=0; i<cells.length;i++) {
       var p = [];
       for (let j=0; j<cells[i].length;j++) {
-      	let cell = cells[i][j];
-      	if (cell instanceof BlackCell) {
-      		p.push(
-	          <div 
+        let cell = cells[i][j];
+        if (cell instanceof BlackCell) {
+          p.push(
+            <div 
               className="square black-square"
               onClick={() => this.handleClick(cell)}
               key={`grd-${i}-${j}`}
             />
-					);
-      	} else if (cell instanceof FixedCell) {
-      		p.push(
-      			<FixedSquare
+          );
+        } else if (cell instanceof FixedCell) {
+          p.push(
+            <FixedSquare
               value={cell.value}
               isInCurrentWord={this.state.currentWord.contains(cell)}
               onClick={() => this.handleClick(cell)}
               key={`grd-${i}-${j}`}
             />
-      		);
-      	} else if (cell instanceof WhiteCell) {
+          );
+        } else if (cell instanceof WhiteCell) {
           p.push(
-      			<WhiteSquare
+            <WhiteSquare
               onClick={() => this.handleClick(cell)}
               value={this.props.entryLookup(i,j)}
               isSelected={_.isEqual(cell, this.state.currentSelection)}
@@ -52,13 +52,13 @@ export default class Grid extends React.Component {
               gridModel={this.props.gridModel}
               key={`grd-${i}-${j}`}
             />
-      		);
-      	}
+          );
+        }
       }
       o.push(<div className="row" key={"grd"+i}>{p}</div>);
     }
-		return (<div className="grid">{o}</div>);
-	}
+    return (<div className="grid">{o}</div>);
+  }
 
   componentDidMount() {
     window.addEventListener("keydown", (event) => this.handleKeyDown(event));
@@ -211,19 +211,19 @@ export default class Grid extends React.Component {
 }
 
 class FixedSquare extends React.Component {
-	render() {
+  render() {
     let extraClass = this.props.isInCurrentWord ? " selected-word" : "";
-		return (
+    return (
       <div className={"square fixed-square" + extraClass}>
         <div className="label">{this.props.label}</div>
         <div className="value" onClick={this.props.onClick}>{this.props.value}</div>
       </div>
     );
-	}
+  }
 }
 
 class WhiteSquare extends React.Component {
-	render() {
+  render() {
     let extraClass =
       this.props.isSelected ? " selected" :
       this.props.isInCurrentWord ? " selected-word" : "";
@@ -233,5 +233,5 @@ class WhiteSquare extends React.Component {
         <div className="value" onClick={this.props.onClick}>{this.props.value}</div>
       </div>
     );
-	}
+  }
 }
